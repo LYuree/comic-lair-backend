be588943e04cfe0d3fb277af4f908faf0136c2bf
LYuree <slymerrow@gmail.com>
2025-02-10 09:34:15 +0700

    Добавил проверку на существование пользователя в базе при регистрации.

--- app/main.py
+++ app/main.py
@@ -11,7 +11,10 @@ app = FastAPI()
 # Настройка CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost", "https://98vxpncx-3000.euw.devtunnels.ms"],
+    allow_origins=["http://localhost:3000",
+                   "http://localhost",
+                   "https://98vxpncx-3000.euw.devtunnels.ms",
+                   "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
